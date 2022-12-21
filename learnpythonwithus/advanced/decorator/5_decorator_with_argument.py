72eb84e84b0d19a6a9604d5f149c67723a6c3c18
syedjafer <syedjafer1997@gmail.com>
2022-12-21 18:58:16 +0530

    updating

--- learnpythonwithus/advanced/decorator/5_decorator_with_argument.py
+++ learnpythonwithus/advanced/decorator/5_decorator_with_argument.py
@@ -12,6 +12,7 @@ def execute_times(num):
     def decorator_repeat(func):
         def inner(*args, **kwargs):
             for _ in range(num):
+                print(func.__name__)
                 func(*args, **kwargs)
         return inner
     return decorator_repeat
