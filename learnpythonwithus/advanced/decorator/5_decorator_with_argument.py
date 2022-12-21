8e3bc3f2fbbbb3eeb12cb5be70972f1669a291ac
syedjafer <syedjafer1997@gmail.com>
2022-12-21 18:57:38 +0530

    updating

---
+++ learnpythonwithus/advanced/decorator/5_decorator_with_argument.py
@@ -0,0 +1,26 @@
+import time
+
+def timeit(func):
+    def inner():
+        start_time = time.perf_counter()
+        func()
+        end_time = time.perf_counter()
+        print(end_time - start_time)
+    return inner
+
+def execute_times(num):
+    def decorator_repeat(func):
+        def inner(*args, **kwargs):
+            for _ in range(num):
+                func(*args, **kwargs)
+        return inner
+    return decorator_repeat
+
+@execute_times(num=2)
+def slow_process():
+    print('Hello World')
+    return True
+
+
+
+slow_process()
